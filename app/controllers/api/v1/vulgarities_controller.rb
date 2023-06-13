1dee37f9a169356bb3b6bb32c973b552110b14d2
Mickael Riss <mendos.prod@gmail.com>
2023-06-13 19:03:03 +0200

    last

--- app/controllers/api/v1/vulgarities_controller.rb
+++ app/controllers/api/v1/vulgarities_controller.rb
@@ -8,16 +8,15 @@ module Api
       # Cette action récupère dans les params le body/ text, applique replace_vulgarities dessus et renvoie le JSON
       def process_dom
         dom = params[:dom]
-        visited_urls = params[:visitedSites]
-        visited_urls.each do |hash|
-          Visit.create!(title: hash[:title], url: hash[:url], date: DateTime.current, profile_id: Profile.last.id)
-        end
+        hash = params[:visitedSite]
+
+        Visit.create!(title: hash[:title], url: hash[:url], date: DateTime.current, profile_id: Profile.last.id)
+
         unless dom
           return render json: { error: "Une erreur s'est produite lors du traitement du DOM." }, status: :unprocessable_entity
         end
 
         @replaced_text = replace_vulgarities(dom: dom)
-        puts "je suis la"
         render json: { modifiedDOM: @replaced_text }
       end
 
@@ -25,10 +24,7 @@ module Api
 
       # Cette action stock notre hash dans vulgarities
       def replace_vulgarities(dom:)
-
-
-        # category = Category.find(current_user.current_category_id)
-        category = Category.first
+        category = Category.find(current_user.current_category_id)
         json_file = File.read(Rails.root.join('public', 'vulgarities.json'))
         hash = JSON.parse(json_file)
         count = 0
@@ -39,12 +35,8 @@ module Api
           when "Faible"
             dom.gsub!(/(#{key})(?!([^<]+)?>)/i) do |match|
             count += 1
-           "<pixy data-word='#{key}'
-                                  data-level='low'
-                                  data-description='#{value['description']}'>
-                                    #{key}
-                                  </pixy>"
-                                  end
+           "<pixy data-level='low'>#{key}</pixy>"
+          end
 
           when "Modéré"
             dom.gsub!(/(#{key})(?!([^<]+)?>)/i) do |match|
@@ -54,7 +46,7 @@ module Api
                                   data-description='#{value['description']}'>
                                     #{value['replace']}
                                     <pixy-explication style='position: absolute; display: none;'>
-                                      <pixy-word>#{key}</pixy-word>
+                                      <pixy-word>#{key.capitalize}</pixy-word><br><br>
                                       <pixy-description>#{value['description']}</pixy-description>
                                     </pixy-explication>
                                   </pixy>"
@@ -66,16 +58,14 @@ module Api
            "<pixy data-level='high' >
                                     #{value['replace']}
                                     <pixy-explication style='position: absolute; display: none;'>
-                                      <pixy-word>#{key}</pixy-word>
+                                      <pixy-word>#{key.capitalize}</pixy-word><br><br>
                                       <pixy-description>#{value['description']}</pixy-description>
                                     </pixy-explication>
                                   </pixy>"
                                 end
           end
         end
-        puts "count: #{count}"
         dom
-
       end
     end
   end
