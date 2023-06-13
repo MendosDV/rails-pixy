07a02bbd63f8f836ac3000753df0c683bcf48bf0
elise rochaix <elise.rochaix@gmail.com>
2023-06-13 14:03:04 +0200

    stylesheet

--- app/controllers/api/v1/vulgarities_controller.rb
+++ app/controllers/api/v1/vulgarities_controller.rb
@@ -17,7 +17,7 @@ module Api
         end
 
         @replaced_text = replace_vulgarities(dom: dom)
-
+        puts "je suis la"
         render json: { modifiedDOM: @replaced_text }
       end
 
@@ -25,40 +25,53 @@ module Api
 
       # Cette action stock notre hash dans vulgarities
       def replace_vulgarities(dom:)
-        category = Category.find(current_user.current_category_id)
-        puts category.name
 
+
+        # category = Category.find(current_user.current_category_id)
+        category = Category.first
         json_file = File.read(Rails.root.join('public', 'vulgarities.json'))
         hash = JSON.parse(json_file)
+        count = 0
+
+        puts "______________________________________"
+        puts category.name
+        puts "______________________________________"
         hash.each do |key, value|
           case category.name
           when "Faible"
-            dom.gsub!(/#{key}/i, "<pixy data-word='#{key}'
-                                  data-level='low'
-                                  data-description='#{value['description']}'>
-                                    #{key}
-                                  </pixy>")
+            dom.gsub!(/#{key}/i) do |a|
+              count += 1; "<pixy data-word='#{key}' data-level='low' data-description='#{value['description']}'>#{key}</pixy>"
+            end
+
+
           when "Modéré"
-            dom.gsub!(/#{key}/i, "<pixy data-word='#{key}'
-                                  data-level='medium'
-                                  data-description='#{value['description']}'>
-                                    #{value['replace']}
-                                    <pixy-explication style='position: absolute; display: none;'>
-                                      <pixy-word>#{key}</pixy-word>
-                                      <pixy-description>#{value['description']}</pixy-description>
-                                    </pixy-explication>
-                                  </pixy>")
+            dom.gsub!(/#{key}/i) do |a|
+              count += 1; "<pixy data-word='#{key}' data-level='medium' data-description='#{value['description']}'>
+              #{value['replace']}
+              <pixy-explication style='position: absolute; display: none;'>
+              <pixy-word>#{key}</pixy-word>
+              <pixy-description>#{value['description']}</pixy-description>
+              </pixy-explication>
+              </pixy>"
+            end
+
           when "Elevé"
-            dom.gsub!(/#{key}/i, "<pixy data-level='high' >
-                                    #{value['replace']}
-                                    <pixy-explication style='position: absolute; display: none;'>
-                                      <pixy-word>#{key}</pixy-word>
-                                      <pixy-description>#{value['description']}</pixy-description>
-                                    </pixy-explication>
-                                  </pixy>")
+
+            dom.gsub!(/#{key}/i) do |a|
+              count += 1; "<pixy data-level='high'>
+                #{value['replace']}
+                <pixy-explication style='position: absolute; display: none;'>
+                  <pixy-word>#{key}</pixy-word>
+                  <pixy-description>#{value['description']}</pixy-description>
+                </pixy-explication>
+              </pixy>"
+            end
+          else
+            puts "PIXYYYYYYYYYYYYYY"
           end
         end
-        dom
+        puts "Nombre de mots changés : #{count}"
+
       end
     end
   end
