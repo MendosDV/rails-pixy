2398a3b9792c20a1e706fc071409a8f41a610da0
youcef haraghi <haraghiy@gmail.com>
2023-06-13 12:29:53 +0200

    add count , picture profil sur popup

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,8 +1,20 @@
+require "cloudinary"
+include CloudinaryHelper
+
 module Api
   module V1
     class UsersController < Api::V1::BaseController
       def index
-        render json: { user: current_user, profiles: current_user.profiles, categories: current_user.profiles.map(&:category_id) }
+        render json: {
+          user: current_user,
+          profiles: current_user.profiles.map do |profile|
+            {
+              infos: profile,
+              picture: cl_image_tag(profile.picture.key)
+            }
+          end
+
+        }
       end
 
       def change_category
