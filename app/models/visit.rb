a548469ef44eedfde176f4e2c9b489fad8027b74
elise rochaix <elise.rochaix@gmail.com>
2023-06-14 14:16:21 +0200

    nbmotschangés

--- app/models/visit.rb
+++ app/models/visit.rb
@@ -1,3 +1,11 @@
 class Visit < ApplicationRecord
   belongs_to :profile
+
+  def self.today
+    where(date: DateTime.current.beginning_of_day..DateTime.current.end_of_day)
+  end
+
+  def self.week
+    where(date: 7.days.ago.beginning_of_day..DateTime.current.end_of_day)
+  end
 end
